a5a6d13fb76e76fd3fc0ac55b727f7ebdd58354b
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-03-11 05:35:09 +0000

    commentモデルとカラムを追加。アソシエーションも設定しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_06_134516) do
+ActiveRecord::Schema.define(version: 2023_03_11_051728) do
 
   create_table "boards", force: :cascade do |t|
     t.string "name"
@@ -20,4 +20,14 @@ ActiveRecord::Schema.define(version: 2023_03_06_134516) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.integer "board_id", null: false
+    t.string "name", null: false
+    t.text "comment", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["board_id"], name: "index_comments_on_board_id"
+  end
+
+  add_foreign_key "comments", "boards"
 end
